3c3d32896ba8a7b6024f69f7411625f8c5b51e01
Erik Rimskog <erik.rimskog@gmail.com>
2017-10-03 23:48:31 +0200

    installed AUCtex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,6 +33,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-item-indent 0)
  '(ac-auto-show-menu nil)
  '(ac-auto-start nil)
  '(ac-candidate-menu-min 0)

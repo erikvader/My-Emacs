a2313f0c34dc16f82ad705d4509cee982cbc24ad
Erik Rimskog <erik.rimskog@gmail.com>
2017-10-30 22:13:53 +0100

    added stuff from evil-plugins
    https://github.com/tarao/evil-plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,6 +21,7 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (add-to-list 'load-path "~/.emacs.d/themes/")
+(add-to-list 'load-path "~/.emacs.d/evil-plugins")
 
 ;;(add-to-list 'load-path "~/.emacs.d/non_elpa/i3-emacs")
 

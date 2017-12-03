6efa0c0881a9cf1755df1743ec59f1a7292a5e10
Erik Rimskog <erik.rimskog@gmail.com>
2017-12-03 15:37:10 +0100

    capitalize-operator, little-word bindings, misc-bindings

--- .emacs.d/evil-plugins/evil-little-word.el
+++ .emacs.d/evil-plugins/evil-little-word.el
@@ -114,13 +114,13 @@ list of categories."
 (evil-define-text-object evil-inner-little-word (count &optional beg end type)
   "Select inner little word."
   (evil-select-inner-object 'evil-little-word beg end type count))
-
-(define-key evil-motion-state-map (kbd "glw") 'evil-forward-little-word-begin)
-(define-key evil-motion-state-map (kbd "glb") 'evil-backward-little-word-begin)
-(define-key evil-motion-state-map (kbd "glW") 'evil-forward-little-word-end)
-(define-key evil-motion-state-map (kbd "glB") 'evil-backward-little-word-end)
-(define-key evil-outer-text-objects-map (kbd "lw") 'evil-a-little-word)
-(define-key evil-inner-text-objects-map (kbd "lw") 'evil-inner-little-word)
+ 
+;; (define-key evil-motion-state-map (kbd "glw") 'evil-forward-little-word-begin)
+;; (define-key evil-motion-state-map (kbd "glb") 'evil-backward-little-word-begin)
+;; (define-key evil-motion-state-map (kbd "glW") 'evil-forward-little-word-end)
+;; (define-key evil-motion-state-map (kbd "glB") 'evil-backward-little-word-end)
+;; (define-key evil-outer-text-objects-map (kbd "lw") 'evil-a-little-word)
+;; (define-key evil-inner-text-objects-map (kbd "lw") 'evil-inner-little-word)
 
 (provide 'evil-little-word)
 ;;; evil-little-word.el ends here

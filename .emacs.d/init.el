42e41e4b983169b40d9a4c246d807b89a6ebdbaf
Erik Rimskog <erik.rimskog@gmail.com>
2018-01-18 23:09:46 +0100

    replaced my crappy region-switch with evil-exchange

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -111,7 +111,7 @@
  '(org-src-window-setup (quote current-window))
  '(package-selected-packages
    (quote
-    (git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags framemove flycheck expand-region evil-surround evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator evil-easymotion disable-mouse diminish dashboard counsel-projectile counsel company-emacs-eclim color-moccur charmap buffer-move browse-kill-ring auctex)))
+    (evil-exchange git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags framemove flycheck expand-region evil-surround evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator evil-easymotion disable-mouse diminish dashboard counsel-projectile counsel company-emacs-eclim color-moccur charmap buffer-move browse-kill-ring auctex)))
  '(perl-continued-brace-offset -3)
  '(perl-continued-statement-offset 3)
  '(perl-indent-level 3)

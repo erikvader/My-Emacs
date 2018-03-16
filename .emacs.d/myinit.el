80d80a36ececc20b0016331545a21b166aefeaff
Erik Rimskog <erik.rimskog@gmail.com>
2018-03-16 23:33:05 +0100

    replaced obsolete flet with cl-labels

--- .emacs.d/myinit.el
+++ .emacs.d/myinit.el
@@ -248,24 +248,24 @@ if SAMELINE then don't move the cursor between lines."
 (defun eriks/avy-binary-search ()
   "Do a binary search to find a character on the current line"
   (interactive)
-  (flet ((b-search (min max)
-                   (when (< min max)
-                     (let* ((avy-keys '(?j ?k ?l))
-                            points
-                            (middle (floor (+ min max) 2)))
-                       (goto-char middle)
-                       ;; special case when only two candidates
-                       (if (not (= middle min))
-                           (setq points (list min middle max))
-                         (setq points (list middle max))
-                         (setq avy-keys '(?j ?l)))
-                       (avy--process points (avy--style-fn avy-style))
-                       (cond
-                        ((= (point) middle))
-                        ((< (point) middle)
-                         (b-search min (1- middle)))
-                        ((> (point) middle)
-                         (b-search (1+ middle) max)))))))
+  (cl-labels ((b-search (min max)
+                        (when (< min max)
+                          (let* ((avy-keys '(?j ?k ?l))
+                                 points
+                                 (middle (floor (+ min max) 2)))
+                            (goto-char middle)
+                            ;; special case when only two candidates
+                            (if (not (= middle min))
+                                (setq points (list min middle max))
+                              (setq points (list middle max))
+                              (setq avy-keys '(?j ?l)))
+                            (avy--process points (avy--style-fn avy-style))
+                            (cond
+                             ((= (point) middle))
+                             ((< (point) middle)
+                              (b-search min (1- middle)))
+                             ((> (point) middle)
+                              (b-search (1+ middle) max)))))))
     (let* ((beg (save-excursion
                   (back-to-indentation)
                   (point)))

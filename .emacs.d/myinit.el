fc2ce922bf4472ab76a2644433278be750fd9d5d
Erik Rimskog <erik.rimskog@gmail.com>
2018-03-16 19:23:02 +0100

    super nice avy binary search

--- .emacs.d/myinit.el
+++ .emacs.d/myinit.el
@@ -245,6 +245,37 @@ if SAMELINE then don't move the cursor between lines."
                 ;; '(?å ?ä ?ö)
                 ))
 
+(defun eriks/avy-binary-search ()
+  "Do a binary search to find a character on the current line"
+  (interactive)
+  (flet ((b-search (min max)
+                   (when (< min max)
+                     (let* ((avy-keys '(?j ?k ?l))
+                            points
+                            (middle (floor (+ min max) 2)))
+                       (goto-char middle)
+                       ;; special case when only two candidates
+                       (if (not (= middle min))
+                           (setq points (list min middle max))
+                         (setq points (list middle max))
+                         (setq avy-keys '(?j ?l)))
+                       (avy--process points (avy--style-fn avy-style))
+                       (cond
+                        ((= (point) middle))
+                        ((< (point) middle)
+                         (b-search min (1- middle)))
+                        ((> (point) middle)
+                         (b-search (1+ middle) max)))))))
+    (let* ((beg (save-excursion
+                  (back-to-indentation)
+                  (point)))
+           (end (save-excursion
+                  (move-end-of-line nil)
+                  (point))))
+      (b-search beg end))))
+
+(define-key eriks-map (kbd ",") 'eriks/avy-binary-search)
+
 ;;;; smartparens
 ;; remove defaults
 (sp-pair "\\\\(" nil :actions :rem)
